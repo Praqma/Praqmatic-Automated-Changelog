838936375b8e408c1be2523ad00927c9a2651d66
Mads Nielsen <man@praqma.net>
2016-04-14 13:39:32 +0200

    Updated pipeline script

--- jenkins-pipeline/pipeline.groovy
+++ jenkins-pipeline/pipeline.groovy
@@ -1,7 +1,7 @@
 REPOSITORY_URL = 'https://github.com/Praqma/Praqmatic-Automated-Changelog.git'
 MAIN_BRANCH = 'master'
 REMOTE_NAME = 'origin'
-JOB_LABELS = 'jenkinsubuntu'
+JOB_LABELS = 'GiJeLiSlave'
 NUM_OF_BUILDS_TO_KEEP = 100
 
 job('1_pretested-integration') {
@@ -9,7 +9,7 @@ job('1_pretested-integration') {
         numToKeep(NUM_OF_BUILDS_TO_KEEP)
     }
 
-    label(JOB_LABELS)
+    label(dockerHostLabel)
 
     properties {
         ownership {
@@ -41,9 +41,13 @@ job('1_pretested-integration') {
         githubPush()
     }
 
-    //First step: Do PAC execute? (No syntax errors?)
+    //First step: Can we build the docker container, and can we run unit tests?
+    //This basically mimics developer behaviour
     steps {
-        shell('ruby pac.rb')
+        shell('''
+docker build -t praqma/pac:snapshot .             
+docker run --entrypoint=/bin/sh --rm -v $(pwd):/data praqma/pac:snapshot -c rake test
+''')
     }
 
 
@@ -58,18 +62,10 @@ job('1_pretested-integration') {
 
     publishers {
         mailer('and@praqma.net', false, false)
-        downstreamParameterized {
-            trigger('2_test') {
-                parameters {
-                    gitRevision()
-                }
-            }
-        }
     }
 }
 
-
-job('2_test') {
+job('2_functional_test') {
     logRotator {
         numToKeep(NUM_OF_BUILDS_TO_KEEP)
     }
@@ -98,55 +94,11 @@ job('2_test') {
         }
     }
 
-    steps {
-        shell('rake test')
-    }
+    triggers {
+        githubPush()
+    }    
 
-    wrappers {
-        buildName('${BUILD_NUMBER}#${GIT_REVISION,length=8}(${GIT_BRANCH})')
-    }
-
-    publishers {
-        mailer('and@praqma.net', false, false)
-        downstreamParameterized {
-            trigger('3_functional_test') {
-                parameters {
-                    gitRevision()
-                }
-            }
-        }
-    }
-}
-
-job('3_functional_test') {
-    logRotator {
-        numToKeep(NUM_OF_BUILDS_TO_KEEP)
-    }
-
-    label(JOB_LABELS)
-
-    properties {
-        ownership {
-            primaryOwnerId('and')
-            coOwnerIds('man')
-        }
-    }
-
-    authorization {
-        permission('hudson.model.Item.Read', 'anonymous')
-    }
-
-    scm {
-        git {
-            remote {
-                name(REMOTE_NAME)
-                url(REPOSITORY_URL)
-            }
-            branch(MAIN_BRANCH)
-            extensions {}
-        }
-    }
-
+    //Since we do 'docker stuff' using rake...i don't know how tests would react if we start running docker in docker
     steps {
         shell('rake functional_test')
     }
@@ -156,7 +108,7 @@ job('3_functional_test') {
     }
 
     publishers {
-        buildPipelineTrigger('4_release') {
+        buildPipelineTrigger('3_release') {
             parameters {
                 gitRevision()
             }
@@ -165,7 +117,7 @@ job('3_functional_test') {
     }
 }
 
-job('4_release') {
+job('3_release') {
     label(JOB_LABELS)
 
     properties {
