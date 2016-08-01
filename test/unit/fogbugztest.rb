e8dd43e03a7a98c610101e2ac5eb28dbe10efd3b
Mads Nielsen <man@praqma.net>
2016-08-01 15:36:08 +0200

    Added first test for #62

---
+++ test/unit/fogbugztest.rb
@@ -0,0 +1,24 @@
+require 'test/unit'
+require_relative "../../lib/model.rb"
+require_relative "../../lib/task.rb"
+require_relative "../../lib/decorators.rb"
+
+class FogBugzTaskSystemTest < Test::Unit::TestCase
+
+  Test::Unit.at_start do  
+  	Core.settings = { :verbosity => 1}
+  end
+
+  #Just testing that we fail to apply and 
+	def test_fogbugz_configuration_should_fail
+		settings = { :name => 'fogbugz', :query_string => 'http://www.praqma.com/#{task_id}', :usr => 'usr_fb', :pw => 'pw_fb' }
+		collection = Model::PACTaskCollection.new
+		task = Model::PACTask.new 'Fogbugz1'
+		task.applies_to = 'fogbugz'
+		collection.add(task)
+		fogbugz = Task::FogBugzTaskSystem.new(settings).apply(collection)
+		assert_false(fogbugz)
+	end
+
+end
+
