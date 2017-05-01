5d10abe8caf50ce254dda3f46240efd4947eaa02
Bue Petersen <bue@praqma.net>
2017-05-01 13:07:10 +0200

    #98 unit test and refactor example

--- lib/model.rb
+++ lib/model.rb
@@ -4,26 +4,34 @@ module Model
   class PACTask
     def initialize(task_id = nil)
       #Lookup key for task management system
-      @task_id = task_id      
+      #We assume that task_id is a string
+      raise ArgumentError.new("PACTask init error, task id is not a string.") if not (task_id == nil or task_id.respond_to?(:to_str))
+      @task_id = task_id
       #Commits tied to this task
-      @commit_collection = PACCommitCollection.new 
+      @commit_collection = PACCommitCollection.new
       #Data from task management systems(s)
       @attributes = { }
       #Key that determines which system(s) we need to look in for data
-      @applies_to = Set.new      
+      @applies_to = Set.new
       #Assigned label. Used in templates so that you can group your tasks using labels.
       @label = Set.new
+
+      # Initialized with nil - will be populated by decorators
+      # FIXME is it default nil ?
+      @data = nil
     end
 
-    def applies_to
-      @applies_to
-    end
-    
-    def data
-	    @data
-    end
+    attr_accessor :commit_collection
+    attr_accessor :task_id
+    attr_accessor :data
+    attr_reader   :label
+    attr_reader   :applies_to
+    attr_reader   :attributes
+    attr_accessor :commits
 
+    # accepts string
     def applies_to=(val)
+      raiseArgumentError.new("apllies_to method accepts only a string") if not val.is_a? String
       @applies_to << val
     end
 
@@ -31,16 +39,12 @@ module Model
       @commit_collection.add(commit)
     end
 
-    def commits
-      @commit_collection.commits
+    def attributes=(value)
+       @attributes = value
     end
 
     def to_s
-      @task_id      
-    end
-
-    def label
-      @label
+      @task_id
     end
 
     #Apppend label to the set of existing labels. This prevents duplicate labels.
@@ -54,31 +58,20 @@ module Model
 
     #We need to override the equals method. A task is uniquely identified by it's identifier (id). This is usually what
     #is used to fetch additonal information from the task management system
-    def ==(other) 
+    def ==(other)
       @task_id == other.task_id
     end
 
     def to_liquid
-      hash = { 
-        'task_id' => @task_id, 
-        'commits' => @commit_collection, 
+      hash = {
+        'task_id' => @task_id,
+        'commits' => @commit_collection,
         'attributes' => attributes,
         'label' => label,
         'data' => data
       }
       hash
     end
-
-    def attributes
-      @attributes
-    end
-
-    def attributes=(value)
-       @attributes = value
-    end
-
-    attr_accessor :commit_collection
-    attr_accessor :task_id
   end
 
   #Model object representing a collection of tasks. Includes logic to append commits to existing tasks.
@@ -88,22 +81,22 @@ module Model
       @tasks = []
     end
 
-    #When you add a task to a collection. It will automatically add new commits to an existing task. You will 
+    #When you add a task to a collection. It will automatically add new commits to an existing task. You will
     #not get duplicate tasks in the collection.
     def add(*t)
-      t.flatten.each do |task|   
+      t.flatten.each do |task|
         unless @tasks.include? task
           @tasks << task
         else
           task.commit_collection.each do |c|
-            @tasks[@tasks.index(task)].commit_collection.add(c) 
-          end     
+            @tasks[@tasks.index(task)].commit_collection.add(c)
+          end
         end
       end
     end
 
     #Enumeartion method. So that PACTaskCollection.each yields a PACTask object
-    def each 
+    def each
       @tasks.each { |task| yield task }
     end
 
@@ -113,7 +106,7 @@ module Model
 
     def unreferenced_commits
       output = @tasks.select { |t| t.task_id.nil? }
-      if output.first.nil? 
+      if output.first.nil?
 		[]
       else
 		output.first.commits
@@ -125,14 +118,14 @@ module Model
     end
 
     #Indexer method for each collected task
-    def [](task_id) 
+    def [](task_id)
       @tasks.select {|t| t.task_id == task_id}.first
     end
 
     #Group each task discovered by it's label
     def by_label
       labelled = Hash.new
-      @tasks.each { |t| 
+      @tasks.each { |t|
         t.label.each { |t_label|
           unless labelled.include? t_label
             labelled[t_label] = []
@@ -145,7 +138,7 @@ module Model
       labelled
     end
 
-    def to_liquid 
+    def to_liquid
       liquid_hash = { 'tasks' => @tasks, 'referenced' => referenced_tasks, 'unreferenced' => unreferenced_commits }
       liquid_hash.merge! by_label
       liquid_hash
@@ -155,7 +148,7 @@ module Model
   end
 
   class PACCommitCollection
-  	def initialize 
+  	def initialize
   		@commits = []
   	end
 
@@ -180,15 +173,15 @@ module Model
       @commits.select{ |c| c.referenced == false }.size
     end
 
-    def health 
+    def health
       count_with.to_f / count
     end
 
-  	def to_liquid 
+  	def to_liquid
   		@commits
   	end
 
-  	attr_accessor :commits 
+  	attr_accessor :commits
   end
 
 	class PACCommit
@@ -196,25 +189,25 @@ module Model
   		@sha = sha
   		@message = message
       @referenced = false
-      @date = date      		
+      @date = date
   	end
 
-  	def to_liquid 
-  		{ 
+  	def to_liquid
+  		{
         'sha' => @sha,
-        'message' => @message, 
-        'header' => header, 
+        'message' => @message,
+        'header' => header,
         'referenced' => referenced,
         'shortsha' => shortsha
       }
   	end
 
-  	def ==(other) 
-  		@sha == other.sha  	
+  	def ==(other)
+  		@sha == other.sha
   	end
 
-    def header      
-      @message.split(/\n/).first      
+    def header
+      @message.split(/\n/).first
     end
 
     #Get default abbreviation from Git
@@ -226,7 +219,7 @@ module Model
     #returned for unmatch commits. That is a PACTask with an id of 'nil'.
     def matchtask(regex, split = nil)
       tasks = []
-      regex.each do |r|        
+      regex.each do |r|
         @message.scan(eval(r['pattern'])).each do |arr|
           if split.nil?
             task = PACTask.new(arr[0])
@@ -240,12 +233,12 @@ module Model
               task.add_commit(self)
               task.label = r['label']
               self.referenced = true
-              tasks << task              
+              tasks << task
             end
           end
         end
       end
-      
+
       tasks
     end
 
--- test/unit/modeltest.rb
+++ test/unit/modeltest.rb
@@ -5,12 +5,42 @@ require_relative "../../lib/decorators.rb"
 
 class ModelTest < Test::Unit::TestCase
 
+
+	# Testing new PAC task creation
+	# but not the trivial get/setters
+	def test_PACTask_initialize
+
+		# A PAC task initializes with a task_id, either nil or something that makese
+		# sense later in the system. This means a string.
+		tc = Model::PACTask.new()
+		assert_nil(tc.task_id,"Task initialized without task id should task_id nil")
+
+		# Shouldn't reaise argument error as we expect nil or string
+		assert_nothing_raised(ArgumentError) {
+			tc = Model::PACTask.new('issuekey')
+			tc = Model::PACTask.new()
+		}
+	end
+
+
+	# Applies to method is setting the tasks systems the PAC tasks have data in
+	def test_PACtask_applies_to_method
+		tc = Model::PACTask.new()
+
+
+		assert_equal(Set.new,tc.applies_to(),"Applies to is default empty set.")
+		# Applies to take a string matching the entry for task system in the config file
+		tc.applies_to='mytasksystem'
+		tasksystem = Set.new ['mytasksystem']
+		assert_equal(tasksystem,tc.applies_to(),"Applies to tasks system not 'mytasksystem' as expected.")
+	end
+
 	def test_model_task_collection
 		tc = Model::PACTaskCollection.new
-		arr_of_tasks = [ Model::PACTask.new(1), Model::PACTask.new(2)]
+		arr_of_tasks = [ Model::PACTask.new("1"), Model::PACTask.new("2")]
 		tc.add(arr_of_tasks)
 
-		ta = Model::PACTask.new(3)
+		ta = Model::PACTask.new("3")
 		tc.add(ta)
 		tc.add(ta)
 		assert_true(tc.length == 3)
@@ -21,10 +51,10 @@ class ModelTest < Test::Unit::TestCase
 		commit1 = Model::PACCommit.new('abcd1234')
 		commit1.referenced = true
 		commit2 = Model::PACCommit.new('abcd1234abcd')
-		commit3 = Model::PACCommit.new('abcd12341235')		
+		commit3 = Model::PACCommit.new('abcd12341235')
 		cc.add(commit1)
 		cc.add(commit2,commit3)
-		
+
 		assert_true(cc.count_with  == 1)
 		assert_true(cc.count_without == 2, "Commit list must contain 2 unreferenced commits, it contained #{cc.count_without}")
 		assert_equal(33, (cc.health*100).to_i, "We expect the health to be 33, it was #{(cc.health*100).to_i}")
@@ -38,7 +68,7 @@ class ModelTest < Test::Unit::TestCase
 
 		task1 = Model::PACTask.new('task1')
 		task1.add_commit(commit1)
-		
+
 		task11 = Model::PACTask.new('task1')
 		task11.add_commit(commit2)
 
@@ -59,13 +89,13 @@ class ModelTest < Test::Unit::TestCase
 		assert_nil(tc['task4'])
 	end
 
-	def test_decorator_module 
+	def test_decorator_module
 		task11 = Model::PACTask.new('STACI-2')
 		#The collection of tasks is totally decoupled from the task system itself. Only requirement is that
 		#you should be able to 'fetch' the required data from a task, given the id of the object. How that is done
 		#is an implementaion detail.
 		task_c = Model::PACTaskCollection.new
-		task_c.add(task11)						
+		task_c.add(task11)
 		assert_false(task11.attributes.has_key?('data'))
 		task11.extend(JiraTaskDecorator)
 		#Notice how that 'task11' now responds to the attributes call with the data field (empty at the moment)
@@ -92,10 +122,10 @@ class ModelTest < Test::Unit::TestCase
 
 
 		group2.add_commit(commit2)
-		
+
 		tc_grouped.add(group1)
 		tc_grouped.add(group2)
-		
+
 		assert_equal(3, tc_grouped.by_label["STACI"].length)
 		assert_equal(1, tc_grouped.by_label["LUCI"].length)
 
