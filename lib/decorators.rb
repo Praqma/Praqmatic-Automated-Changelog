c9cddc60a1ea799519a01c4207818de10d1f153a
Mads Nielsen <man@praqma.net>
2019-08-16 14:18:34 +0200

    More precise error description

--- lib/decorators.rb
+++ lib/decorators.rb
@@ -1,32 +1,32 @@
 #This is my proposed way of adding a decorator. Use this to populate additonal attributes to the PACTask.
 #We can use the key value pairs easily in liquid templates. This means that when liquid requests the 'attributes' of
-#a particular task...we can fetch the data here in the decorator 
+#a particular task...we can fetch the data here in the decorator
 module JiraTaskDecorator
-  require 'net/http' 
+  require 'net/http'
   require 'uri'
   require 'json'
-  require 'base64'	
-  require_relative 'logging' 
+  require 'base64'
+  require_relative 'logging'
 
-  attr_accessor :data 
+  attr_accessor :data
 
-  def fetch(query_string, usr, pw) 
-    expanded = eval('"'+query_string+'"')    
+  def fetch(query_string, usr, pw)
+    expanded = eval('"'+query_string+'"')
 	  uri = URI.parse(expanded)
-    
+
     begin
       res = DecoratorUtils.query(uri, usr, pw)
-    rescue Exception
-      raise Exception, "Unknown host error for task with id #{task_id} on url #{expanded}"
-    end 
+    rescue Exception => net_error
+      raise Exception, "Unknown host error for task with id #{task_id} on url #{expanded}\n#{net_error}"
+    end
 
     unless res.is_a? Net::HTTPOK
       raise Exception, "Failed to fetch task with id #{task_id} on url #{expanded} return code was #{res.code}"
     end
 
     begin
-      Logging.verboseprint(3, "[PAC] Got the following data from #{expanded}: #{res.body}") 
-      @data = parse(res.body) 
+      Logging.verboseprint(3, "[PAC] Got the following data from #{expanded}: #{res.body}")
+      @data = parse(res.body)
       Logging.verboseprint(1, "[PAC] Fetched the following from Jira: #{@data}")
       @data
     rescue JSONError
@@ -40,20 +40,20 @@ module JiraTaskDecorator
   end
 
   def attributes
-    super.merge!(      
-      { 
+    super.merge!(
+      {
         'data' => @data
       }
     )
   end
-  
+
 end
 
 module TracTaskDecorator
   require 'trac4r'
 
   def self.trac_instance
-    @@trac_instance    
+    @@trac_instance
   end
 
   def self.trac_instance=(trac)
@@ -62,26 +62,26 @@ module TracTaskDecorator
 
   def fetch
     begin
-      unless task_id.nil? 
+      unless task_id.nil?
         ticket = TracTaskDecorator.trac_instance.tickets.get task_id.to_i
         @data = { :summary => ticket.summary, :status => ticket.status, :description => ticket.description }
         Logging.verboseprint(1, "[PAC] Fetched the following from Trac: #{@data}")
-        @data        
+        @data
       end
     rescue Trac::TracException => e
       raise Exception, "[PAC] The ticket with the id #{task_id} not found in Trac"
-    end    
+    end
   end
 
   def attributes
     super.merge!(
-      { 
+      {
         'data' => @data
       }
-    )    
+    )
   end
 
-  attr_accessor :data  
+  attr_accessor :data
 end
 
 module FogbugzTaskDecorator
@@ -89,13 +89,13 @@ module FogbugzTaskDecorator
   require 'xmlsimple'
 
   def fetch(query_string)
-    expanded = eval('"'+query_string+'"')    
-    uri = URI.parse(expanded)    
+    expanded = eval('"'+query_string+'"')
+    uri = URI.parse(expanded)
     res = DecoratorUtils.query(uri)
     Logging.verboseprint(3, "[PAC] Got the following data from #{expanded}: #{res.body}")
     @data = XmlSimple.xml_in res.body
     Logging.verboseprint(1, "[PAC] Fetched the following from FogBugz: #{@data}")
-    @data    
+    @data
   end
 
   def attributes
@@ -103,7 +103,7 @@ module FogbugzTaskDecorator
   end
 end
 
-module DecoratorUtils extend self  
+module DecoratorUtils extend self
 
   def query(uri, usr = nil, pw = nil)
     req = Net::HTTP::Get.new(uri)
@@ -121,8 +121,8 @@ module DecoratorUtils extend self
     else
       res = Net::HTTP.start(uri.hostname, uri.port) { |http|
         http.request(req)
-      }        
-    end    
+      }
+    end
   end
 
 end
